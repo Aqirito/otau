3142f937966c8931db60cdc303fc7638d961f44e
Tachibana Aqirito <aqil.rahmat@kinabalumakers.dev>
2023-06-26 23:10:24 +0800

    UPDATE: compare remote and local branch

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import requests
 
-def compare_commits(repo_owner, repo_name, base_commit, head_commit):
-    # GitHub API endpoint for comparing commits
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/compare/{base_commit}...{head_commit}"
-
+def is_remote_ahead(repo_owner, repo_name, branch_name, local_commit_sha):
+    # GitHub API endpoint to get the latest commit SHA of the remote branch
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{branch_name}"
+    
     # Send GET request to the GitHub API
     response = requests.get(url)
-
+    
     if response.status_code == 200:
-        comparison = response.json()
-        total_commits = comparison['total_commits']
-        files_changed = len(comparison['files'])
-
-        print(f"Total commits between {base_commit} and {head_commit}: {total_commits}")
-        print(f"Total files changed between {base_commit} and {head_commit}: {files_changed}")
+        remote_commit_sha = response.json()["commit"]["sha"]
+        
+        # Compare the commit SHAs
+        return remote_commit_sha != local_commit_sha
     else:
-        print("Error: Unable to compare commits.")
+        print(f"Error: Unable to fetch remote branch information. Status code: {response.status_code}")
+        return False
 
 # Example usage
 repo_owner = "Aqirito"
-repo_name = "sbx-voice-cloning-booth"
-base_commit = "main"  # Replace with the base commit SHA
-head_commit = "resample-audio"  # Replace with the head commit SHA
+repo_name = "otau"
+branch_name = "main"
+local_commit_sha = "main"  # Replace with your local commit SHA
 
-compare_commits(repo_owner, repo_name, base_commit, head_commit)
+result = is_remote_ahead(repo_owner, repo_name, branch_name, local_commit_sha)
+print(f"Is remote branch ahead of local branch? {result}")
