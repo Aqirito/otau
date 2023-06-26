ec1145d10198cc45bea40166b25ed07154ea1f67
Tachibana Aqirito <aqil.rahmat@kinabalumakers.dev>
2023-06-26 23:47:51 +0800

    UPDATE: branch detection

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import requests
-from git_log import get_commit_sha
+from git import Repo
+import os
+current_directory = os.getcwd()
+print("Current Directory:", current_directory)
 
-def is_remote_ahead(repo_owner, repo_name, branch_name, local_commit_sha):
+repo_owner = "Aqirito"
+repo_name = "otau"
+branch_name = "main"
+repo = Repo(current_directory)
+
+def is_remote_ahead(repo_owner, repo_name, branch_name):
     # GitHub API endpoint to get the latest commit SHA of the remote branch
     url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{branch_name}"
+
+    local_commit_sha = get_commit_sha(branch_name)  # Replace with your local commit SHA
     
     # Send GET request to the GitHub API
     response = requests.get(url)
     
     if response.status_code == 200:
         remote_commit_sha = response.json()["commit"]["sha"]
+        remote_commit_branch = response.json()["name"]
+        print(f"Commit SHA of REMOTE:'{remote_commit_branch}': {remote_commit_sha}")
         
         # Compare the commit SHAs
         return remote_commit_sha != local_commit_sha
@@ -17,11 +29,22 @@ def is_remote_ahead(repo_owner, repo_name, branch_name, local_commit_sha):
         print(f"Error: Unable to fetch remote branch information. Status code: {response.status_code}")
         return False
 
-# Example usage
-repo_owner = "Aqirito"
-repo_name = "otau"
-branch_name = "main"
-local_commit_sha = get_commit_sha  # Replace with your local commit SHA
+def get_commit_sha(branch_name):
+    branch = repo.heads[branch_name]
+    commit = branch.commit
+    commit_sha = commit.hexsha
+    print(f"Commit SHA of LOCAL: '{branch_name}': {commit_sha}")
+    return commit_sha
 
-result = is_remote_ahead(repo_owner, repo_name, branch_name, local_commit_sha)
-print(f"Is remote branch ahead of local branch? {result}")
+def git_pull():
+
+    result = is_remote_ahead(repo_owner, repo_name, branch_name)
+    print(f"Is remote branch ahead of local branch? {result}")
+
+    if result == True:
+        repo.git.pull()
+    else:
+        print("Already up to date")
+
+
+git_pull()
