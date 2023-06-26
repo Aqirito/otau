3ccd81afd333474e7a233cdb34d3bff1c5e51f69
Tachibana Aqirito <aqil.rahmat@kinabalumakers.dev>
2023-06-27 00:05:38 +0800

    UPDATE: main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import requests
 from git import Repo
 import os
+import json
+
 current_directory = os.getcwd()
 print("Current Directory:", current_directory)
 
-repo_owner = "Aqirito"
-repo_name = "otau"
-branch_name = "main"
 repo = Repo(current_directory)
 
-def is_remote_ahead(repo_owner, repo_name, branch_name):
-    # GitHub API endpoint to get the latest commit SHA of the remote branch
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{branch_name}"
+with open(os.path.join(current_directory, "config.json"), "r") as f:
+    f.seek(0)  # Move to the beginning of the file
+    config = json.loads(f.read())
+    repo_owner = config['repo_owner']
+    repo_name = config['repo_name']
+    remote_branch_name = config['remote_branch_name']
+    local_branch_name = config['local_branch_name']
 
-    local_commit_sha = get_commit_sha(branch_name)  # Replace with your local commit SHA
+def is_remote_ahead(repo_owner, repo_name, remote_branch_name, local_branch_name):
+    # GitHub API endpoint to get the latest commit SHA of the remote branch
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/branches/{remote_branch_name}"
+
+    local_commit_sha = get_commit_sha(remote_branch_name)  # Replace with your local commit SHA
     
     # Send GET request to the GitHub API
     response = requests.get(url)
@@ -29,16 +36,16 @@ def is_remote_ahead(repo_owner, repo_name, branch_name):
         print(f"Error: Unable to fetch remote branch information. Status code: {response.status_code}")
         return False
 
-def get_commit_sha(branch_name):
-    branch = repo.heads[branch_name]
+def get_commit_sha(local_branch_name):
+    branch = repo.heads[local_branch_name]
     commit = branch.commit
     commit_sha = commit.hexsha
-    print(f"Commit SHA of LOCAL: '{branch_name}': {commit_sha}")
+    print(f"Commit SHA of LOCAL: '{local_branch_name}': {commit_sha}")
     return commit_sha
 
 def git_pull():
 
-    result = is_remote_ahead(repo_owner, repo_name, branch_name)
+    result = is_remote_ahead(repo_owner, repo_name, remote_branch_name, local_branch_name)
     print(f"Is remote branch ahead of local branch? {result}")
 
     if result == True:
