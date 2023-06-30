428bb9bb6fe93f3340f4f3a70e8ac1fe187b4f5c
Aqirito <aqirito@github.com>
2023-06-30 20:44:09 +0800

    UPDATE: test ota

--- sensors/CO/config.py
+++ sensors/CO/config.py
@@ -2,7 +2,7 @@ MQTT_SERVER = "device_ip_address"
 WIFI_SSID = "wifi_network"
 WIFI_PASSWORD = "wifi_password"
 
-SENSOR_TYPE = "sensor_type" # CO, DHT, NO2, SO2, PM25, O3
+SENSOR_TYPE = "CO" # CO, DHT, NO2, SO2, PM25, O3
 SENSOR_IP = "sensor_ip_address" # Sensor IP address (check in the README)
 TOPIC_PUB = "topic_for_publish_mqtt" # CO_sensor, DHT_sensor, NO2_sensor, SO2_sensor, PM25_sensor, O3_sensor
 TOPIC_SUB = "topic_for_subscribe_mqtt" # topic for subscribe incoming message from MQTT server example: "server"
